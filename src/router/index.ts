1657c04a95e21e38190c76d622f281270e785e1a
Mehdi Bourahla <mehdibrhl4@gmail.com>
2023-03-08 10:46:10 -0500

    🔥 Totally Blank project

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/MainView.vue";
+import MainView from "../views/MainView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "main",
+    component: MainView,
   },
 ];
 
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home"></div>
+  <div>Hello World!</div>
 </template>
 
 <script lang="ts">
