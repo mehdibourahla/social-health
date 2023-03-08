8eba6cf756d3432c890fc412a7428cdcc8ef8e12
Mehdi Bourahla <mehdibrhl4@gmail.com>
2023-03-08 11:36:18 -0500

    🚨 Configure Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,14 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
-    ecmaVersion: 2020,
+    parser: "babel-eslint"
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  }
 };
