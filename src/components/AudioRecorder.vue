bffb5a951f20dbc06c996f71bcd15221457359b7
Mehdi Bourahla <mehdibrhl4@gmail.com>
2023-03-18 14:36:42 -0400

    ♻️ Refactor Audio Recorder

--- src/components/AudioRecorder.vue
+++ src/components/AudioRecorder.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="d-flex justify-center float w-100">
     <v-btn
-      :loading="loading > 0"
-      :disabled="loading > 0"
+      :loading="loading"
+      :disabled="loading"
       @click="toggleRecording"
       size="x-large"
       :icon="isRecording ? 'mdi-stop' : 'mdi-microphone'"
@@ -14,12 +14,16 @@
 
 <script>
 import { defineComponent } from "vue";
-import axios from "axios";
 
 export default defineComponent({
+  props: {
+    loading: {
+      type: Boolean,
+      required: true,
+    },
+  },
   data() {
     return {
-      loading: 0,
       mediaRecorder: null,
       audioChunks: [],
       isRecording: false,
@@ -37,22 +41,13 @@ export default defineComponent({
       this.mediaRecorder.addEventListener("stop", () => {
         const audioBlob = new Blob(this.audioChunks, { type: "audio/ogg" });
         this.saveAudio(audioBlob);
-        this.whisper(audioBlob);
       });
     });
   },
   methods: {
-    async saveAudio(blob) {
-      this.loading++;
-      this.audioUrl = URL.createObjectURL(blob);
-      const transcriptions = await this.whisper(blob);
-      const chatGPT = await this.askGPT(transcriptions.text);
-      this.$emit("update-records", {
-        id: Date.now(),
-        transcriptions,
-        chatGPT,
-      });
-      this.loading--;
+    saveAudio(audio) {
+      this.audioUrl = URL.createObjectURL(audio);
+      this.$emit("get-audio", audio);
     },
     toggleRecording() {
       if (this.isRecording) {
@@ -63,49 +58,6 @@ export default defineComponent({
         this.isRecording = true;
         this.mediaRecorder.start();
       }
-    },
-    async whisper(blob) {
-      const url = "https://api.openai.com/v1/audio/transcriptions";
-      const token = process.env.VUE_APP_OPENAI_TOKEN; // Replace TOKEN with your OpenAI API token
-      const model = "whisper-1";
-
-      const formData = new FormData();
-      formData.append("file", blob, "audio.mp3");
-      formData.append("model", model);
-
-      const response = await axios.post(url, formData, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "multipart/form-data",
-        },
-      });
-
-      return response.data;
-    },
-
-    async askGPT(transcription) {
-      const url = "https://api.openai.com/v1/chat/completions";
-      const token = process.env.VUE_APP_OPENAI_TOKEN; // Replace TOKEN with your OpenAI API token
-      const model = "gpt-3.5-turbo";
-      const prompt =
-        "Answer using a number only. Positive social interactions are interactions that promote the wellbeing of everyone involved, and lead to the ongoing development of social skills that encourage the parties to communicate respectfully and feel accepted by each other. Use this definition rate on a scale from 1-100 the following social interaction: ";
-      const formData = {
-        messages: [
-          { role: "user", content: prompt + "'" + transcription + "'" },
-        ],
-        model: model,
-      };
-
-      const response = await axios.post(url, formData, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      });
-
-      const content = response.data.choices[0].message.content;
-
-      return parseInt(content.match(/\d+/)[0]);
     },
   },
 });
--- src/views/SocialHealthView.vue
+++ src/views/SocialHealthView.vue
@@ -6,7 +6,8 @@
       @delete-transcript="deleteTranscript"
     ></audio-transcripts>
 
-    <audio-recorder @update-records="addTranscript"> </audio-recorder>
+    <audio-recorder :loading="loading" @get-audio="addTranscript">
+    </audio-recorder>
   </div>
 </template>
 
@@ -15,6 +16,7 @@ import AudioRecorder from "@/components/AudioRecorder.vue";
 import AudioTranscripts from "@/components/AudioTranscripts.vue";
 import { defineComponent } from "vue";
 import { Storage } from "@capacitor/storage";
+import axios from "axios";
 
 export default defineComponent({
   components: { AudioRecorder, AudioTranscripts },
@@ -22,6 +24,7 @@ export default defineComponent({
 
   data() {
     return {
+      loading: false,
       localAudio: [],
     };
   },
@@ -38,12 +41,22 @@ export default defineComponent({
         value: JSON.stringify(this.localAudio),
       });
     },
-    async addTranscript(transcript) {
-      this.localAudio.unshift(transcript);
+    async addTranscript(audio) {
+      this.loading = true;
+      const transcriptions = await this.whisper(audio);
+      const chatGPT = await this.askGPT(transcriptions.text);
+
+      this.localAudio.unshift({
+        id: Date.now(),
+        transcriptions,
+        chatGPT,
+      });
+
       await Storage.set({
         key: "localAudio",
         value: JSON.stringify(this.localAudio),
       });
+      this.loading = false;
     },
     async refresh() {
       const ret = await Storage.get({ key: "localAudio" });
@@ -51,6 +64,50 @@ export default defineComponent({
       if (this.localAudio.length > 0) {
         this.localAudio.sort((a, b) => b.id - a.id);
       }
+    },
+
+    async whisper(blob) {
+      const url = "https://api.openai.com/v1/audio/transcriptions";
+      const token = process.env.VUE_APP_OPENAI_TOKEN; // Replace TOKEN with your OpenAI API token
+      const model = "whisper-1";
+
+      const formData = new FormData();
+      formData.append("file", blob, "audio.mp3");
+      formData.append("model", model);
+
+      const response = await axios.post(url, formData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      return response.data;
+    },
+
+    async askGPT(transcription) {
+      const url = "https://api.openai.com/v1/chat/completions";
+      const token = process.env.VUE_APP_OPENAI_TOKEN; // Replace TOKEN with your OpenAI API token
+      const model = "gpt-3.5-turbo";
+      const prompt =
+        "Answer using a number only. Positive social interactions are interactions that promote the wellbeing of everyone involved, and lead to the ongoing development of social skills that encourage the parties to communicate respectfully and feel accepted by each other. Use this definition rate on a scale from 1-100 the following social interaction: ";
+      const formData = {
+        messages: [
+          { role: "user", content: prompt + "'" + transcription + "'" },
+        ],
+        model: model,
+      };
+
+      const response = await axios.post(url, formData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      const content = response.data.choices[0].message.content;
+
+      return parseInt(content.match(/\d+/)[0]);
     },
   },
 });
